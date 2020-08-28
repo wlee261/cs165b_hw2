7c73013f2f583e360d2a5e11aaf089793d9a0b0e
wlee261 <38049741+wlee261@users.noreply.github.com>
2020-08-28 11:26:20 -0700

    Add files via upload

--- hw2.py
+++ hw2.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
-
 import math
-#import numpy as np
+import numpy as np
 from time import time
 from collections import Counter
 
@@ -29,6 +27,10 @@ def read_data(path):
         feature3.append(line_in_traininglist[2])
         feature4.append(line_in_traininglist[3])
         data.labels.append(line_in_traininglist[4].rstrip())
+        if(data.labels[i] == "Iris-setosa"):
+            data.labels[i] = 1
+        else:
+            data.labels[i] = 0
     
     data.features.append(feature1)
     data.features.append(feature2)
@@ -41,24 +43,20 @@ def read_data(path):
 def dot_kf(u, v):
     """
     The basic dot product kernel returns u*v.
-
     Args:
         u: list of numbers
         v: list of numbers
-
     Returns:
         u*v
     """
     # TODO: implement the kernel function
-    return dot(u,v)
+    return np.dot(u,v)
 
 def poly_kernel(d):
     """
     The polynomial kernel.
-
     Args:
         d: a number
-
     Returns:
         A function that takes two vectors u and v,
         and returns (u*v+1)^d.
@@ -71,10 +69,8 @@ def poly_kernel(d):
 def exp_kernel(s):
     """
     The exponential kernel.
-
     Args:
         s: a number
-
     Returns:
         A function that takes two vectors u and v,
         and returns exp(-||u-v||/(2*s^2))
@@ -98,11 +94,17 @@ class Perceptron:
 
     def train(self, data):
         # TODO: Main function - train the perceptron with data
-        errors = 0
-        converged = false
-        while converged = false:
+        trainpoint = [0, 0, 0, 0]
+        converged = False
+        while converged == False:
+            converged = True
             for i in range(len(data.labels)):
-                if(
+                trainpoint[0] = data.features[0][i]
+                trainpoint[1] = data.features[1][i]
+                trainpoint[2] = data.features[2][i]
+                trainpoint[3] = data.features[3][i]
+                if(self.update(trainpoint, data.labels[i])):
+                    converged = False
         return
         
 
@@ -110,35 +112,74 @@ class Perceptron:
 
         """
         Updates the parameters of the perceptron, given a point and a label.
-
         Args:
             point: a list of numbers
             label: either 1 or -1
-
         Returns:
             True if there is an update (prediction is wrong),
             False otherwise (prediction is accurate).
         """
         # TODO
+        if(self.predict(point) != label):
+            self.MissedLabels.append(label)
+            self.MissedPoints.append(point)
+            is_mistake = True
+        else:
+            is_mistake = False
         return is_mistake
+
+    def getScore(self, point):
+        for x in range(len(self.MissedLabels)):
+            score =  score + self.MissedLabels[x]*kf(point, self.MissedPoints[x])
+            return score
+
+
 
     def predict(self, point):
         """
         Given a point, predicts the label of that point (1 or -1).
         """
+        score = self.getScore(point)
+        if float(score) > 0:
+            label = 1
+        else:
+            label = -1
         # TODO
-        return
+        return label
 
     def test(self, data):
         predictions = []
+        point = [0, 0, 0, 0]
         # TODO: given data and a perceptron - return a list of integers (+1 or -1).
         # +1 means it is Iris Setosa
         # -1 means it is not Iris Setosa
+        for x in range(len(data.labels)):
+            point[0] = data.features[0][x]
+            point[1] = data.features[1][x]
+            point[2] = data.features[2][x]
+            point[3] = data.features[3][x]
+            predictions.append(self.predict(point))
+
         return predictions
 
 
 # Feel free to add any helper functions as needed.
 def main():
+    p = Perceptron(dot_kf, 0)
+    traindata = Data()
+    testdata = Data()
+    traindata = read_data("hw2_train.txt")
+    testdata = read_data("hw2_test.txt")
+    p.train(traindata)
+    result = []
+    result = test(traindata)
+    correct = 0
+    total = 0
+    for i in range(len(traindata)):
+        if result[i] == traindata.labels[i]:
+            correct = correct + 1
+        total = total + 1
+    print(correct/total)
     return
     
 
@@ -146,4 +187,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
